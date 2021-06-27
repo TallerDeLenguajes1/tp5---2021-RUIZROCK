4c66a781b2f7a098ef59aa5d07a70c86799c902b
RUIZROCK <sergioruizrock@gmail.com>
2021-06-27 01:15:29 -0300

    TP en curso, falta lo que seria la parte de forms

---
+++ JuegoRol/JuegoRol/JuegoRol/Modelos/CrearPlayerGenerico.cs
@@ -0,0 +1,131 @@
+﻿using JuegoRol.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoRol
+{
+    class CrearPlayerGenerico
+    {
+        static Random aleatorio = new Random();
+
+//metodo para crear personaje
+        public static Personajes CrearPersonaje(List<string> nombreAleatorio)
+        {
+
+            Personajes nuevoJugador = new Personajes();
+
+            nuevoJugador.Personaje = generarTipo();
+            nuevoJugador.Nombre = generarNombre(nombreAleatorio);
+            nuevoJugador.Apodo = generarApodo();
+            nuevoJugador.FechaDeNacimiento = generadorFechaNacimiento();
+            nuevoJugador.Edad = generadorEdad(nuevoJugador.FechaDeNacimiento);
+            nuevoJugador.Salud = 100;
+
+            nuevoJugador.Velocidad = genererVelocidad();
+            nuevoJugador.Destreza = genererDestreza();
+            nuevoJugador.Fuerza = genererFuerza();
+            nuevoJugador.Nivel = 1;
+            nuevoJugador.Armadura = genererArmadura();
+
+            return nuevoJugador;
+        }
+        /*****************************************************************************************/
+        /*****************************************************************************************/
+        /*****************************************************************************************/
+
+        //metodos para la Obtención de los Datos
+
+        //tipo
+        static tipo generarTipo()
+        {
+
+            int num = aleatorio.Next(5);
+            tipo tipo;
+
+            switch (num)
+            {
+                case 0:
+                    tipo = tipo.ogro;
+                    break;
+                case 1:
+                    tipo = tipo.duende;
+                    break;
+                case 2:
+                    tipo = tipo.burro;
+                    break;
+                case 3:
+                    tipo = tipo.nieri;
+                    break;
+                default:
+                    tipo = tipo.ehamigo;
+                    break;
+            }
+            return tipo;
+        }
+
+                        //nombre
+        private static string generarNombre(List<string> nombrePokemones)
+        {
+            int numNombre = aleatorio.Next(20);
+            return nombrePokemones[numNombre];
+        }
+
+                        //apodo
+        private static string generarApodo()
+        {
+            string[] apodos = { "Pepillo", "Cepillin","riñon fijo", "sasha", "claudia", "vrayan", "pachano" };
+
+            int numApodo = aleatorio.Next(5);
+
+            return apodos[numApodo];
+        }
+
+                        //fecha de nacimiento
+        private static DateTime generadorFechaNacimiento()
+        {
+            DateTime inicio = new DateTime(1721, 1, 1);
+            int range = (DateTime.Today - inicio).Days;
+            return inicio.AddDays(aleatorio.Next(range)).Date;
+        }
+
+
+                            //edad
+        public static int generadorEdad(DateTime fechaNacimiento)
+        {
+            return DateTime.Today.Year - fechaNacimiento.Year;
+        }
+
+        /*****************************************************************************************/
+        /*****************************************************************************************/
+        /*****************************************************************************************/
+        
+        //metodos para la Obtencion de las Caracteristicas
+
+        //velocidad
+        public static int genererVelocidad()
+        {
+            return aleatorio.Next(1, 11);
+        }
+
+                        //destreza
+        public static int genererDestreza()
+        {
+            return aleatorio.Next(1, 6);
+        }
+
+                        //fuerza
+        public static int genererFuerza()
+        {
+            return aleatorio.Next(1, 11);
+        }
+
+                        //armadura
+        public static int genererArmadura()
+        {
+            return aleatorio.Next(1, 11);
+        }
+    }
+}
